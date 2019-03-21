4c0c5a49fbee9051ecc2eee7738a0174a7fbbee0
h-n-y <hnyelek@gmail.com>
2019-03-20 21:04:12 -0600

    Add Intro section

---
+++ src/MainPage/IntroSection/IntroSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PT from 'prop-types';
+
+class IntroSection extends React.Component {
+
+    render() {
+        return (
+            <section id="intro-section">
+                <h1>
+                    <span> Hi, </span> 
+                    <span> I'm Hans.  </span>
+                </h1> 
+                <p>
+                    <span>
+                        I'm a growing 
+                    </span> 
+                    <span>
+                        <strong> front-end </strong> and <strong> iOS </strong> developer.
+                    </span>
+                    <span>
+                        Scroll down to see examples
+                    </span>
+                    <span>
+                        of my work: 
+                    </span>
+                </p>
+            </section>
+        );
+    }
+}
+
+IntroSection.propTypes = {
+};
+
+export default IntroSection;
---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PT from 'prop-types';
+
+import IntroSection from './IntroSection/IntroSection';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <IntroSection />
+        );
+    }
+}
+
+MainPage.propTypes = {
+};
+
+export default MainPage;
