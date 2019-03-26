aa372bfb65325d3f9daa564a5488969792e8e2b9
h-n-y <hnyelek@gmail.com>
2019-03-26 16:22:50 -0600

    Add roulette images

--- src/MainPage/sections/RouletteSection/RouletteSection.js
+++ src/MainPage/sections/RouletteSection/RouletteSection.js
@@ -3,6 +3,11 @@ import React from 'react';
 import SectionTitle from '../SectionTitle/SectionTitle';
 import AppLink from '../../AppLink/AppLink';
 
+import rouletteDesignDesaturated from '../../../assets/img/roulette-design-desaturated.png';
+import rouletteDesign1 from '../../../assets/img/roulette-example-1.png';
+import rouletteDesign2 from '../../../assets/img/roulette-example-2.png';
+import rouletteDesign3 from '../../../assets/img/roulette-example-3.png';
+
 import {
     APP_LINK_TYPE,
 } from '../../../constants/constants';
@@ -15,10 +20,34 @@ const APP_KEYWORDS = [
 
 class RouletteSection extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            rouletteImageLoadedFlags: [ false, false, false ],
+        };
+
+        this.handleImageLoad = this.handleImageLoad.bind(this);
+    }
+
+    handleImageLoad(rouletteImageIndex) {
+
+        const flags = this.state.rouletteImageLoadedFlags;
+        const flagsUpdated = flags.slice(0, flags.length);
+        flags[rouletteImageIndex] = true;
+
+        this.setState({ rouletteImageLoadedFlags: flagsUpdated });
+    }
+
     render() {
         return (
 
             <div id="roulette-section">
+
+                <div id="roulette-bg-design-wrapper">
+                    <img src={rouletteDesignDesaturated} alt="roulette design"
+                        onLoad={this.handleImageLoad} /> 
+                </div>
                  
                 <div className="primary">
                     <SectionTitle
@@ -49,10 +78,12 @@ class RouletteSection extends React.Component {
                 <div className="image-cards">
                      
                     <div className="image-card">
-                         
+                        <img src={rouletteDesign1} alt="roulette example"
+                            onLoad={this.handleImageLoad} /> 
                     </div>
                     <div className="image-card">
-                         
+                        <img src={rouletteDesign2} alt="roulette example"
+                            onLoad={this.handleImageLoad} /> 
                     </div>
                 </div>
             </div>
