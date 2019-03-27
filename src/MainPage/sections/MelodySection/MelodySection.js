0bfbbd4bcb73191c0e354ba1cf38413bfb4f10f6
h-n-y <hnyelek@gmail.com>
2019-03-27 11:48:19 -0600

    Add Melody images; Change Footer section bg color

--- src/MainPage/sections/MelodySection/MelodySection.js
+++ src/MainPage/sections/MelodySection/MelodySection.js
@@ -3,6 +3,10 @@ import React from 'react';
 import SectionTitle from '../SectionTitle/SectionTitle';
 import AppLink from '../../AppLink/AppLink';
 
+// images
+import trebleClef from '../../../assets/svg/treble.svg';
+import baseClef from '../../../assets/svg/base.svg';
+
 import {
     APP_LINK_TYPE,
     SECTION_TITLE_POSITION,
@@ -20,20 +24,29 @@ class MelodySection extends React.Component {
 
         return (
             <div id="melody-section">
-                <SectionTitle 
-                    title="Melody"
-                    keywords={APP_KEYWORDS}
-                    position={SECTION_TITLE_POSITION.right}
-                />
+                <div id="melody-section-bg">
+                    <img src={trebleClef} alt="treble clef" id="treble-clef"/> 
+                    <img src={baseClef} alt="base clef" id="base-clef"/> 
+                </div>
+                <div className="primary">
+                    <SectionTitle 
+                        title="Melody"
+                        keywords={APP_KEYWORDS}
+                        position={SECTION_TITLE_POSITION.right}
+                        displayedOverDarkBackground={true}
+                    />
 
-                <p className="app-description">
-                    Search artists, tracks, and lyrics with this simple, responsive prototype web app. 
-                </p>
-                     
-                <AppLink
-                    title="Search"
-                    linkType={APP_LINK_TYPE.simple}
-                />
+                    <p className="app-description">
+                        Search artists, tracks, and lyrics with this simple, responsive prototype web app. 
+                    </p>
+                         
+                    <div className="app-link-wrapper">
+                        <AppLink
+                            title="Search"
+                            linkType={APP_LINK_TYPE.simple}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
