ff3c4e917a587275f690ebe34a3eab3a409a41ba
h-n-y <hnyelek@gmail.com>
2019-04-18 00:51:49 -0600

    Updates

---
+++ precache-manifest.6aa004c5c95e2b3d45c972349c2a47e6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "872e6ff06a5223cf91e9eed4b0052635",
+    "url": "/my-portfolio/static/media/treble.872e6ff0.svg"
+  },
+  {
+    "revision": "fa2eabbf41daa431d885",
+    "url": "/my-portfolio/static/css/main.506dc266.chunk.css"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "133c5d20364e12e5b0c2",
+    "url": "/my-portfolio/static/js/2.9e84d214.chunk.js"
+  },
+  {
+    "revision": "48c2634f5fdefb73fe764a34b738f9f4",
+    "url": "/my-portfolio/static/media/roulette-design.48c2634f.png"
+  },
+  {
+    "revision": "91fa4272162eae5af7f69f6718522583",
+    "url": "/my-portfolio/static/media/roulette-design-desaturated.91fa4272.png"
+  },
+  {
+    "revision": "89dda1da6c00e78603b37a829730deae",
+    "url": "/my-portfolio/static/media/roulette-example-1.89dda1da.png"
+  },
+  {
+    "revision": "12ee952f0e3c10d29ec5a475885923d7",
+    "url": "/my-portfolio/static/media/roulette-example-2.12ee952f.png"
+  },
+  {
+    "revision": "3faddf725260485b7c3dae989d0f55b6",
+    "url": "/my-portfolio/static/media/roulette-example-3.3faddf72.png"
+  },
+  {
+    "revision": "fa2eabbf41daa431d885",
+    "url": "/my-portfolio/static/js/main.895e6a1e.chunk.js"
+  },
+  {
+    "revision": "def0f6de483ae418fdd1860b3ed945b7",
+    "url": "/my-portfolio/static/media/base.def0f6de.svg"
+  },
+  {
+    "revision": "69e8d969e5610938bfb8405b3af90aa2",
+    "url": "/my-portfolio/static/media/sheet-music.69e8d969.svg"
+  },
+  {
+    "revision": "f56146a9a4d07f78bdd563227cf48131",
+    "url": "/my-portfolio/static/media/oxo-icon.f56146a9.png"
+  },
+  {
+    "revision": "db1ebfebd6ba361ad2ea913e4afb3214",
+    "url": "/my-portfolio/static/media/oxo-desktop.db1ebfeb.png"
+  },
+  {
+    "revision": "cbcbc3650edabdd3fe6aff7b7e21e186",
+    "url": "/my-portfolio/static/media/Resume.cbcbc365.pdf"
+  },
+  {
+    "revision": "9565b05825b42c217f9152c2fbadb653",
+    "url": "/my-portfolio/static/media/Cantarell-Regular.9565b058.ttf"
+  },
+  {
+    "revision": "340f1f0557525e193416dbfbc1f3a704",
+    "url": "/my-portfolio/static/media/EBGaramond-SemiBold.340f1f05.ttf"
+  },
+  {
+    "revision": "9c351431da4f06d0c105241d86f80cb1",
+    "url": "/my-portfolio/index.html"
+  }
+];
